967fb2e76676c0a24d33c699846972a050723efe
Nathanael White <narhanaelw99@gmail.com>
2021-04-22 23:42:06 -0500

    added on click and saveEvents function

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
-// 
+// var eventText = $("#eventText");
+var saveButton = $("#save-btn");
+var eventEl = $("#eventHour");
+
+
+
+// create a function to save events to specific textarea on the page
+// when user types in textarea, then clicks the save btn, the text appends to the page
+
+// event has to save on click (.click) of the saveIcon. On click, get the value of textarea and run saveEvents
+$(document).ready(function () {
+
+  // call when button is clicked
+  $(saveButton).click(function () {
+    var eventText = $("#eventText").val();
+    console.log(eventText);
+  });
+});
+
+var saveEvents = function () { 
+  localStorage.setItem("eventText", eventText);
+};
+
+// loadEvents to keep eventText on screen after refresh
+// JSON the events array to local storage
+var loadEvents = function() {
+    var savedEvents = localStorage.getItem("eventText");
+    // if nothing in localStorage, then return
+    if (!savedEvents) {
+        return false;
+    };
+}
+// saveEvents function to save eventText to local storage
+// to take out of localStorage, I have to JSON.parse the item
+
+
